57888f08039e93a8f73a48d51f279f3552dd3fd5
Юхименко Анастасия Эдуардовна <mail@yuhimenko-anastasia.ru>
2026-05-15 12:05:16 +0000

    Dev

---
+++ MeetPoint/MeetPoint/MeetPointApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetPointApp.swift
+//  MeetPoint
+//
+//  Created by Anastasia Yukhimenko on 15.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct MeetPointApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
